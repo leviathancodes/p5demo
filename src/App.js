1b18ae32def21fc5f0712fb80ea451e6fcd68732
lhuddlesto <lance299@gmail.com>
2022-10-03 12:35:57 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useRef } from 'react';
+import p5 from 'p5';
 
+function sketch(p) {
+    p.setup = function() {
+      p.createCanvas(window.innerWidth, window.innerHeight, p.WEBGL);
+      p.describe('A 3D white ellipsoid');
+    }
+
+    p.draw = function() {
+      p.ellipsoid(100,50,100);
+    }
+}
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const p5ContainerRef = useRef();
+    useEffect(() => {
+        const p5Instance = new p5(sketch, p5ContainerRef.current);
+        return () => {
+            p5Instance.remove();
+        }
+    }, []);
+    return (
+        <div className="App" ref={p5ContainerRef} />
+    );
 }
 
 export default App;
